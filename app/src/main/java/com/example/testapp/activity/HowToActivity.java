4b37f41ec00e39a4f9ccddee1b7098b941fd09b9
ihatetmat <kchk0628@naver.com>
2024-12-07 15:47:51 +0900

    test

--- app/src/main/java/com/example/testapp/activity/HowToActivity.java
+++ app/src/main/java/com/example/testapp/activity/HowToActivity.java
@@ -1,9 +1,10 @@
 package com.example.testapp.activity;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+
 import androidx.appcompat.app.AppCompatActivity;
+
 import com.example.testapp.R;
 
 public class HowToActivity extends AppCompatActivity {
@@ -16,10 +17,7 @@ public class HowToActivity extends AppCompatActivity {
         findViewById(R.id.returnButton).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // 이전 화면으로 돌아가기
-                Intent intent = new Intent(HowToActivity.this, StartActivity.class);
-                startActivity(intent);
-                finish(); // 현재 액티비티 종료
+                finish();
             }
         });
     }
